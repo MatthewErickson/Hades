187fae29068852b4696bfd4bb99c7c0bcecd571e
MatthewErickson <ericks51@students.wwu.edu>
2017-03-19 12:14:12 -0700

    Reformat / clean up

--- hades/static/hades/js/base.js
+++ hades/static/hades/js/base.js
@@ -4,16 +4,3 @@ $("form").submit(function() {
 	});
 	return true;
 });
-
-$(document).on('touchend', function(event){
-  var $clickedOn = $(event.target),
-      $collapsableItems = $('.collapse'),
-      isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
-      isLink = ($clickedOn.closest('a').length == 1),
-      isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
-  if( (!isToggleButton && isLink) || isOutsideNavbar ) {
-    $collapsableItems.each(function(){
-      $(this).collapse('hide');
-    });
-  }
-});
